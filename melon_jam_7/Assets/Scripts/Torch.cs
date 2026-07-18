657943d05970d645705225093376218752b23a6a
Satchel R Diaz <94930613+satcheldiaz16@users.noreply.github.com>
2026-07-18 17:55:03 -0400

    pushing again cause what the what

--- melon_jam_7/Assets/Scripts/Torch.cs
+++ melon_jam_7/Assets/Scripts/Torch.cs
@@ -6,6 +6,8 @@ public class Torch : MonoBehaviour
     [SerializeField] ParticleSystem particles;
     [SerializeField] GameObject _light;
     [SerializeField] AudioSource sfx;
+    [SerializeField] GameObject visual_target;
+    [SerializeField] GameObject audio_target;
     [SerializeField] float lit_duration = 20f;
     float put_out_timer = 0;
     bool lit;
@@ -15,6 +17,8 @@ public class Torch : MonoBehaviour
         _light.SetActive(true);
         put_out_timer = lit_duration;
         lit = true;
+        visual_target.SetActive(true);
+        audio_target.SetActive(true);
         sfx.Play();
     }
     void Update()
@@ -32,6 +36,8 @@ public class Torch : MonoBehaviour
         particles.Stop();
         _light.SetActive(false);
         lit = false;
+        visual_target.SetActive(false);
+        audio_target.SetActive(false);
         sfx.Stop();
     }
 }
