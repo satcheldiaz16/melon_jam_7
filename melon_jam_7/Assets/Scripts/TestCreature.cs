8d4bad8def1d20ef057f348567206e80e5232c3e
Satchel R Diaz <94930613+satcheldiaz16@users.noreply.github.com>
2026-07-18 01:42:29 -0400

    done for the day, started on some prop assets, w textures, pathfinding, yadayadayada

--- melon_jam_7/Assets/Scripts/TestCreature.cs
+++ melon_jam_7/Assets/Scripts/TestCreature.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -9,11 +10,20 @@ public class TestCreature : MonoBehaviour
 {
     [SerializeField] NavMeshAgent nav_agent;
     [SerializeField] PlayerController player;
-    [SerializeField] float wander_location_radius = 25f;
     [SerializeField] CreatureState state;
-    [SerializeField] float wander_speed;
+    [Header("Pursuit")]
     [SerializeField] float pursuit_speed;
+    float pursuit_timer = 0;
+    [SerializeField] float time_btwn_vision_checks = 2f;
+    [Header("Vision")]
     [SerializeField] VisionCone vision;
+    [SerializeField] VisualTarget current_visual_target;
+    [Header("Wander")]
+    [SerializeField] float wander_speed;
+    [SerializeField] float wander_location_radius = 25f;
+    [SerializeField] float min_idle_time = 1f;
+    [SerializeField] float max_idle_time = 5f;
+    float wander_timer = 0;
     void OnEnable()
     {
         if (vision)
@@ -30,39 +40,81 @@ public class TestCreature : MonoBehaviour
     }
     void SpotTarget(VisualTarget target)
     {
+        if(current_visual_target!=null && !target.is_player) return;
+
+        current_visual_target = target;
+
         SetPathToPosition(target.transform.position);
+        nav_agent.speed = pursuit_speed;
+        state = CreatureState.pursuit;
     }
     void Start()
     {
-        TestPathToPlayer();
+        
     }
     void EnterStunnedState()
     {
         state = CreatureState.stunned;
-
-
     }
     void Update()
     {
-        /*
+        switch (state)
+        {
+            case CreatureState.wander: UpdateWander(); break;
+            case CreatureState.pursuit: UpdatePursuit(); break;
+        }
+    }
+    void UpdatePursuit()
+    {
         if (HasArrived())
         {
-            NavMeshPath path = new NavMeshPath();
-            nav_agent.CalculatePath(PickWanderLocation(), path);
-            nav_agent.SetPath(path);
+            pursuit_timer = time_btwn_vision_checks;
+            state = CreatureState.wander;
+            current_visual_target = null;
+            return;
         }
-        */
+
+        if(pursuit_timer > 0)
+        {
+            pursuit_timer -= Time.deltaTime;
+            return;
+        }
+
+        if(current_visual_target != null && vision.CanSee(current_visual_target.transform))
+        {
+            SetPathToPosition(current_visual_target.transform.position);
+            Debug.Log("Checking target again");
+        }
+        Debug.Log("End of pursuit loop");
+        pursuit_timer = time_btwn_vision_checks;
+    }
+    void UpdateWander()
+    {
+        if(wander_timer > 0)
+        {
+            wander_timer -= Time.deltaTime;
+            return;
+        }
+
+        if (HasArrived())
+        {
+            nav_agent.speed = wander_speed;
+            int roll = Random.Range(0, 4);
+            if(roll < 2) // stand around
+            {
+                wander_timer = Random.Range(min_idle_time, max_idle_time);
+            }
+            else //wander
+            {
+                Vector3 wander_pos = PickWanderLocation();
+                SetPathToPosition(wander_pos);
+            }
+        }
     }
     void SetPathToPosition(Vector3 position)
     {
         NavMeshPath path = new NavMeshPath();
         nav_agent.CalculatePath(position, path);
-        nav_agent.SetPath(path);
-    }
-    void TestPathToPlayer()
-    {
-        NavMeshPath path = new NavMeshPath();
-        nav_agent.CalculatePath(player.transform.position, path);
         nav_agent.SetPath(path);
     }
     bool HasArrived()
--- melon_jam_7/Assets/Scripts/VisionCone.cs
+++ melon_jam_7/Assets/Scripts/VisionCone.cs
@@ -1,41 +1,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class VisionCone : MonoBehaviour
 {
-    [SerializeField] GameObject owner;
-    [SerializeField] List<Collider> spotted = new List<Collider>();
     public List<VisualTarget> targets = new List<VisualTarget>();
+    [SerializeField] float view_radius = 15f;
+    [SerializeField] float view_angle = 60f;   // full cone angle
+    [SerializeField] LayerMask target_mask;
+    [SerializeField] LayerMask obstacle_mask;
     public event System.Action<VisualTarget> TargetSpotted;
-    void OnTriggerEnter(Collider other)
+    void Start() => InvokeRepeating(nameof(VisionCheck), Random.Range(0f, 0.2f), 0.2f);
+    void VisionCheck()
     {
-        Debug.Log("Enter");
-        AttemptToAdd(other);
-    }
-    void OnTriggerStay(Collider other)
-    {
-        Debug.Log("Stay");
-        AttemptToAdd(other);
-    }
-    void AttemptToAdd(Collider other)
-    {
-        if(owner && other.gameObject==owner) return;
+        Collider[] hits = Physics.OverlapSphere(transform.position, view_radius, target_mask);
 
-        if (spotted.Contains(other)) return;
-
-        spotted.Add(other);
-
-        TargetSpotted?.Invoke(other.gameObject.GetComponent<VisualTarget>());
-    }
-    void OnTriggerExit(Collider other)
-    {
-        AttemptToRemove(other);
-    }
-    void AttemptToRemove(Collider other)
-    {
-        if (spotted.Contains(other))
+        foreach(Collider col in hits)
         {
-            spotted.Remove(other);
+            if (CanSee(col.gameObject.transform))
+            {
+                Debug.Log("lets get to the bottom of this");
+                TargetSpotted?.Invoke(col.gameObject.GetComponent<VisualTarget>());
+            }
         }
+    }
+    public bool CanSee(Transform target)
+    {
+        Vector3 to_target = target.position - transform.position;
+
+        if (to_target.sqrMagnitude > view_radius * view_radius) return false;
+        if (Vector3.Angle(transform.forward, to_target) > view_angle * 0.5f) return false;
+
+        // occlusion
+        RaycastHit hit;
+        bool occluded = !Physics.Raycast(transform.position, to_target.normalized, out hit, to_target.magnitude, obstacle_mask);
+        //Debug.Log(hit.collider.gameObject.name);
+        return occluded;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (transform == null) return;
+
+        // range sphere
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, view_radius);
+
+        // cone edges
+        Gizmos.color = Color.yellow;
+        Vector3 left  = Quaternion.Euler(0f, -view_angle * 0.5f, 0f) * transform.forward;
+        Vector3 right = Quaternion.Euler(0f,  view_angle * 0.5f, 0f) * transform.forward;
+
+        Gizmos.DrawRay(transform.position, left  * view_radius);
+        Gizmos.DrawRay(transform.position, right * view_radius);
+
+        // arc
+        Handles.color = new Color(1f, 1f, 0f, 0.1f);
+        Handles.DrawSolidArc(transform.position, Vector3.up, left, view_angle, view_radius);
     }
 }
