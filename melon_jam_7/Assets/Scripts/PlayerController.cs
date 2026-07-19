6eecec9b848128372646ee477285b9639058722f
Satchel R Diaz <94930613+satcheldiaz16@users.noreply.github.com>
2026-07-19 15:13:11 -0400

    removed extra proj files, brithing tree but actually, fixed jump exploit

--- melon_jam_7/Assets/Scripts/PlayerController.cs
+++ melon_jam_7/Assets/Scripts/PlayerController.cs
@@ -11,6 +11,8 @@ public enum PlayerMovementState
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] Transform camera_pos;
+    [SerializeField] LayerMask ground_mask;
+    [SerializeField] float jump_slope_limit = 55f;
     [Header("Movement")]
     [SerializeField] CharacterController character_controller;
     [SerializeField] AudioSource jump_sfx;
@@ -90,7 +92,7 @@ public class PlayerController : MonoBehaviour
 
         Vector3 final_vel = move_dir * GetMoveSpeed();
 
-        if (jump_input_pressed && character_controller.isGrounded)
+        if (jump_input_pressed && character_controller.isGrounded && IsGrounded())
         {
             vertical_vel = Mathf.Sqrt(jump_height * -2.0f * gravity);
             jump_sfx.Play();
@@ -105,6 +107,19 @@ public class PlayerController : MonoBehaviour
         character_controller.Move(final_vel * Time.deltaTime);
 
         camera_pos.localPosition = new Vector3(0, GetCameraHeight(), 0);
+    }
+    bool IsGrounded()
+    {
+        float radius = character_controller.radius * 0.9f;
+        Vector3 origin = transform.position + character_controller.center;
+        float dist = (character_controller.height / 2f) - character_controller.radius + character_controller.skinWidth + 0.1f;
+
+        if (Physics.SphereCast(origin, radius, Vector3.down, out RaycastHit hit, dist, ground_mask, QueryTriggerInteraction.Ignore))
+        {
+            float angle = Vector3.Angle(hit.normal, Vector3.up);
+            return angle <= jump_slope_limit;
+        }
+        return false;
     }
     void HandleWalkSFX()
     {
